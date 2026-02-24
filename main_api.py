1032529ad88e296ada5bc472b5edbfbcce4fdea2
hasanbahcecii <hasan.bahceci021@gmail.com>
2026-02-24 15:57:12 +0300

    api tested

--- main_api.py
+++ main_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np 
 import torch
+import random
 import torch.nn as nn
 import joblib
 
@@ -35,18 +36,26 @@ model = GRUNet(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE)
 model.load_state_dict(torch.load("gru_model.pth", map_location="cpu"))
 model.eval()
 
-# load scaler
+
+#load scaler 
+scaler_X = joblib.load("scaler_X.save") 
 scaler_y = joblib.load("scaler_y.save")
+
 
 # API endpoint
 @app.post("/predict")
 def predict(data: InputData):
+
+    """
+        It makes a traffic volume prediction by using the 24 hours input sequence.
+    """
     input_seq = np.array(data.sequence).astype(np.float32)
 
     if input_seq.shape != (24, 7):
         return {"error": "Dimension is not correct, expected size (24, 7)"}
-    
-    input_tensor = torch.tensor(input_seq).unsqueeze(0) # (1, 24, 7)
+     # normalize input with scaler_X 
+    input_seq_scaled = scaler_X.transform(input_seq)
+    input_tensor = torch.tensor(input_seq_scaled).unsqueeze(0) # (1, 24, 7)
 
     with torch.no_grad():
         prediction = model(input_tensor)
@@ -56,3 +65,17 @@ def predict(data: InputData):
     predicted_value = float(prediction_org[0][0])
 
     return {"predicted_traffic_volume": predicted_value}
+
+# test generator
+sample_sequence = []
+for i in range(24):
+    temp = random.uniform(280, 300) # kelvin 
+    rain = 0
+    snow = 0
+    clouds = random.randint(0, 100)
+    hour = 1
+    dayofweek = random.randint(0, 6)
+    month = 10
+    sample_sequence.append([temp, rain, snow, clouds, hour, dayofweek, month])
+
+print(sample_sequence)    
